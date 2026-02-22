c181169eaaa3b0145e0626d2c0eec31893dea9a1
bulbul64 <bbb46483@gmail.com>
2026-02-22 16:36:00 +0600

    get all-jobs  interview rejected

--- machine.js
+++ machine.js
@@ -30,3 +30,5 @@
  console.log(allJobs)
  return allJobs
 }
+
+
--- main.js
+++ main.js
@@ -6,3 +6,9 @@ import { jobsRender } from "./machine.js"
 const jobsContainer = document.getElementById("jobs-container")
 jobsContainer.innerHTML = jobsRender(jobs)
 
+//  get all-jobs  interview rejected 
+
+const allBtn = document.getElementById("all-jobs")
+const interview = document.getElementById("interview")
+const rejected = document.getElementById(" ")
+
